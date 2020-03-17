fdafb822b7e1c27694f34deb37a1d22e5f482a82
alperyyildiz <50810809+alperyyildiz@users.noreply.github.com>
2020-03-18 01:33:19 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+pip install tensorflow
+pip install tf-nightly
+pip install Pillow
+pip install scikit-learn
+pip install numpy
+pip install pandas
+pip install keras
+pip install xlrd
+python tune1.py
