0cfb91978970520dbee101179b0fe21a56cca45d
alperyyildiz <50810809+alperyyildiz@users.noreply.github.com>
2020-03-21 14:51:41 +0300

    Create run2.sh

---
+++ run2.sh
@@ -0,0 +1,12 @@
+pip install --upgrade pip
+pip install tensorflow
+pip install tf-nightly
+pip install Pillow
+pip install scikit-learn
+pip install numpy
+pip install pandas
+pip install keras
+pip install xlrd
+pip install hyperopt
+pip install statsmodels
+python tune2.py
